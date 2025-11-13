815253288958ce03bb3199e8a09f324827ea17eb
rashidnabiev003 <rashidkhonnabiev@gmail.com>
2025-11-13 17:34:39 +0300

    +1hard problem

---
+++ Сodeforces/279B_div2.py
@@ -0,0 +1,15 @@
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+
+left = 0
+current_sum = 0
+max_books = 0
+
+for right in range(n):
+    current_sum += a[right]
+    while current_sum > t and left <= right:
+        current_sum -= a[left]
+        left += 1
+    max_books = max(max_books, right - left + 1)
+
+print(max_books)
